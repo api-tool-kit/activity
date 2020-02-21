62993a033320bfaebb460eb18b37a3d078c7dbe6
Mike Amundsen <mamund@yahoo.com>
2020-02-21 11:32:20 -0500

    fix up project

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 /*****************************************
-<<<<<<< HEAD
-// bigco, inc activity
+// activity service for BigCo, Inc.
 // root of the service API
 // 2020-02-01 : mamund
-=======
- * account service for BigCo, Inc.
- * 2019-01 mamund
->>>>>>> 8a57f003db37321d4aef320b5710f207c938148c
  *****************************************/
  
 var express = require('express');
 var app = express();
-<<<<<<< HEAD
 var cors = require('cors');
 
 var resources = require('./darrt/resources');
@@ -26,10 +20,4 @@ app.use('/',resources);
 
 // start listening for requests
 app.listen(port, () => console.log(`listening on port ${port}!`));
-=======
-var account = require('./account');
-var port = process.env.PORT || 8282;
- 
-app.use('/account',account);
-app.listen(port, () => console.log(`account svc listening on port ${port}!`));
->>>>>>> 8a57f003db37321d4aef320b5710f207c938148c
+
